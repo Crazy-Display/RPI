26d172dfea575d9989595abd9eaff06f0af4c597
Eduardo Amorós López <73888835+Edu026@users.noreply.github.com>
2023-11-09 17:41:49 +0100

    Add files via upload

---
+++ RPI/src/main/java/com/project/RPIServer.java
@@ -0,0 +1,71 @@
+package com.project;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import org.java_websocket.WebSocket;
+import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.server.WebSocketServer;
+import org.json.JSONObject;
+
+public class RPIServer extends WebSocketServer {
+
+    static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+    public RPIServer (int port) {
+        super(new InetSocketAddress(port));
+    }
+
+    @Override
+    public void onStart() {
+        // Quan el servidor s'inicia
+        String host = getAddress().getAddress().getHostAddress();
+        int port = getAddress().getPort();
+        System.out.println("WebSockets server running at: ws://" + host + ":" + port);
+        System.out.println("Type 'exit' to stop and exit server.");
+        setConnectionLostTimeout(0);
+        setConnectionLostTimeout(100);
+    }
+
+    @Override
+    public void onOpen(WebSocket conn, ClientHandshake handshake) {
+       
+    }
+
+    @Override
+    public void onClose(WebSocket conn, int code, String reason, boolean remote) {
+
+    }
+
+    @Override
+    public void onMessage(WebSocket conn, String message) {
+        
+    }
+
+    @Override
+    public void onError(WebSocket conn, Exception ex) {
+        // Quan hi ha un error
+        ex.printStackTrace();
+    }
+
+
+    public void runServerBucle () {
+        boolean running = true;
+        try {
+            System.out.println("Starting server");
+            start();
+            while (running) {
+                String line;
+                line = in.readLine();
+                if (line.equals("exit")) {
+                    running = false;
+                }
+            } 
+            System.out.println("Stopping server");
+            stop(1000);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }  
+    }
+}
